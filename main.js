e0f188608eae9a3786e0f1864fd5adfd1a8bedec
shivam-namdeo <92042834+shivam-namdeo@users.noreply.github.com>
2022-08-10 23:04:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+x = 0;
+y = 0;
+screenwidth=0;
+screenheight=0;
+draw_apple = "";
+apple="";
+speak_data="";
+to_number="";
+function preload()
+{
+  apple=loadImage("apple.png");
+}
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+to_number=Number(content);
+if(Number.)
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
